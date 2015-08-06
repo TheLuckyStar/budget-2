d25361a77b2f4190cf56f466df4f3a0d01373621
Simon Dubois <simon@dubandubois.com>
2015-08-06 20:21:02 +0200

    Fix home page layout

--- resources/views/home/guest.blade.php
+++ resources/views/home/guest.blade.php
@@ -23,75 +23,84 @@
         </div>
     </div>
 
-    <div class="col-md-6">
-        <div class="panel panel-info">
-            <div class="panel-heading text-right">
-                <i class="fa fa-fw fa-envelope fa-5x pull-left"></i>
-                <i>@lang('home.guest.benefits.subtitle')</i>
-                <h3>@lang('home.guest.benefits.title')</h3>
-            </div>
-            <ul class="list-group">
-                @foreach(trans('home.guest.benefits.content') as $content)
-                    <li class="list-group-item">{{ $content }}</li>
-                @endforeach
-            </ul>
-            <div class="panel-footer">
-                @lang('home.guest.benefits.footer')
-            </div>
-        </div>
-    </div>
+    <div class="col-md-12">
+        <div class='row'>
 
-    <div class="col-md-6">
-        <div class="panel panel-info">
-            <div class="panel-heading text-right">
-                <i class="fa fa-fw fa-desktop fa-5x pull-left"></i>
-                <i>@lang('home.guest.usages.subtitle')</i>
-                <h3>@lang('home.guest.usages.title')</h3>
+            <div class="col-md-6">
+                <div class="panel panel-info">
+                    <div class="panel-heading text-right">
+                        <i class="fa fa-fw fa-envelope fa-5x pull-left"></i>
+                        <i>@lang('home.guest.benefits.subtitle')</i>
+                        <h3>@lang('home.guest.benefits.title')</h3>
+                    </div>
+                    <ul class="list-group">
+                        @foreach(trans('home.guest.benefits.content') as $content)
+                            <li class="list-group-item">{{ $content }}</li>
+                        @endforeach
+                    </ul>
+                    <div class="panel-footer">
+                        @lang('home.guest.benefits.footer')
+                    </div>
+                </div>
             </div>
-            <ul class="list-group">
-                @foreach(trans('home.guest.usages.content') as $content)
-                    <li class="list-group-item">{{ $content }}</li>
-                @endforeach
-            </ul>
-            <div class="panel-footer">
-                @lang('home.guest.usages.footer')
-            </div>
-        </div>
-    </div>
 
-    <div class="col-md-6">
-        <div class="panel panel-info">
-            <div class="panel-heading text-right">
-                <i class="fa fa-fw fa-star fa-5x pull-left"></i>
-                <i>@lang('home.guest.features.subtitle')</i>
-                <h3>@lang('home.guest.features.title')</h3>
+            <div class="col-md-6">
+                <div class="panel panel-info">
+                    <div class="panel-heading text-right">
+                        <i class="fa fa-fw fa-desktop fa-5x pull-left"></i>
+                        <i>@lang('home.guest.usages.subtitle')</i>
+                        <h3>@lang('home.guest.usages.title')</h3>
+                    </div>
+                    <ul class="list-group">
+                        @foreach(trans('home.guest.usages.content') as $content)
+                            <li class="list-group-item">{{ $content }}</li>
+                        @endforeach
+                    </ul>
+                    <div class="panel-footer">
+                        @lang('home.guest.usages.footer')
+                    </div>
+                </div>
             </div>
-            <ul class="list-group">
-                @foreach(trans('home.guest.features.content') as $content)
-                    <li class="list-group-item">{{ $content }}</li>
-                @endforeach
-            </ul>
-            <div class="panel-footer">
-                @lang('home.guest.features.footer')
-            </div>
-        </div>
-    </div>
 
-    <div class="col-md-6">
-        <div class="panel panel-info">
-            <div class="panel-heading text-right">
-                <i class="fa fa-fw fa-user-secret fa-5x pull-left"></i>
-                <i>@lang('home.guest.trust.subtitle')</i>
-                <h3>@lang('home.guest.trust.title')</h3>
+        </div>
+        <div class='row'>
+
+            <div class="col-md-6">
+                <div class="panel panel-info">
+                    <div class="panel-heading text-right">
+                        <i class="fa fa-fw fa-star fa-5x pull-left"></i>
+                        <i>@lang('home.guest.features.subtitle')</i>
+                        <h3>@lang('home.guest.features.title')</h3>
+                    </div>
+                    <ul class="list-group">
+                        @foreach(trans('home.guest.features.content') as $content)
+                            <li class="list-group-item">{{ $content }}</li>
+                        @endforeach
+                    </ul>
+                    <div class="panel-footer">
+                        @lang('home.guest.features.footer')
+                    </div>
+                </div>
             </div>
-            <ul class="list-group">
-                @foreach(trans('home.guest.trust.content') as $content)
-                    <li class="list-group-item">{{ $content }}</li>
-                @endforeach
-            </ul>
-            <div class="panel-footer">
-                @lang('home.guest.trust.footer')
+
+            <div class="col-md-6">
+                <div class="panel panel-info">
+                    <div class="panel-heading text-right">
+                        <i class="fa fa-fw fa-user-secret fa-5x pull-left"></i>
+                        <i>@lang('home.guest.trust.subtitle')</i>
+                        <h3>@lang('home.guest.trust.title')</h3>
+                    </div>
+                    <ul class="list-group">
+                        @foreach(trans('home.guest.trust.content') as $content)
+                            <li class="list-group-item">{{ $content }}</li>
+                        @endforeach
+                    </ul>
+                    <div class="panel-footer">
+                        @lang('home.guest.trust.footer')
+                    </div>
+                </div>
             </div>
+
         </div>
     </div>
 
