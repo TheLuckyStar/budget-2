034842518518b7c7631fc2ab48a9a6c610ec6fe8
Simon Dubois <simon@dubandubois.com>
2015-11-18 13:01:48 +0100

    Multi account page : account summaries

--- resources/views/home/authenticated.blade.php
+++ resources/views/home/authenticated.blade.php
@@ -4,21 +4,23 @@
 
     @foreach ($accounts as $account)
         <div class="col-md-4">
-            <div class="panel panel-{{ $account->envelopes_status }}">
+            <div class="panel panel-primary">
 
                 <div class="panel-heading">
-                    {!! $account->link() !!}
+                    <h4>
+                        {!! $account->link() !!}
+                    </h4>
                 </div>
 
                 <ul class="list-group">
 
-                    <li class="list-group-item">
+                    <li class="list-group-item list-group-item-{{ $account->status }}">
                         @lang('account.summary.balance.title', [
                             'balance' => Html::formatPrice($account->balance, $account->currency, true)
                         ])
                     </li>
 
-                    <li class="list-group-item">
+                    <li class="list-group-item list-group-item-{{ $account->envelopes_status }}">
                         @lang('account.summary.envelopes.title', [
                             'balance' => $account->envelopes->isEmpty() ? trans('home.authenticated.noEnvelope') : Html::formatPrice($account->envelopes_balance, $account->currency, true)
                         ])
